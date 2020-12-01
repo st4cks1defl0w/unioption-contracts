9678fbb2369a1a19a668c00da5aa6a65c05647f6
stacksideflow <1@stacksideflow.host>
2020-12-01 22:05:51 +0300

    Add automatic ES contract verification

--- truffle-config.js
+++ truffle-config.js
@@ -1,28 +1,37 @@
 module.exports = {
-  // testing on `truffle develop` for quick iteration
-  networks: {
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`)
-      },
-      // confirmations: 2,
-      network_id: 3,
-      skipDryRun: true
+    // testing on `truffle develop` for quick iteration
+    networks: {
+        ropsten: {
+            provider: function() {
+                return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`)
+            },
+            // confirmations: 2,
+            network_id: 3,
+            skipDryRun: true
+        },
+        development: {
+            host: "127.0.0.1",
+            port: 9545,
+            network_id: "*"
+        },
+        test: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        }
     },
-   development: {
-     host: "127.0.0.1",
-     port: 9545,
-     network_id: "*"
-   },
-   test: {
-     host: "127.0.0.1",
-     port: 7545,
-     network_id: "*"
-   }
-  },
     compilers: {
         solc: {
             version: '0.6.12'
         }
-  }
+    },
+    plugins: [
+        'truffle-plugin-verify'
+    ],
+    verify: {
+        preamble: 'Testnet contracts preamble'
+    },
+    api_keys: {
+        etherscan: process.env.ETHERSCAN_API_KEY
+    },
 };
